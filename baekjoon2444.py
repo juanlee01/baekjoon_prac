f8d8684d2ad31e7bf4cf152d09e59c10bd2f8993
juanlee0166 <juanlee0166@gmai.com>
2025-01-20 02:57:54 +0900

    prac0119.1

--- baekjoon2444.py
+++ baekjoon2444.py
@@ -1,9 +1,15 @@
 n = int(input())
-line = 2 * n - 1
 
 for i in range(1, n):
-    for i in range(n - i, 0, -1):
+    for j in range(1, n - i + 1):
         print(" ", end = "")
-    for i in range(1, i * 2):
+    for k in range(1, i * 2):
         print("*", end = "")
-    print("\n")
+    print("")
+
+for i in range(0, n):
+    for j in range(0, i):
+        print(" ", end = "")
+    for k in range(0, n * 2 - 1 - i * 2):
+        print("*", end = "")
+    print("")
